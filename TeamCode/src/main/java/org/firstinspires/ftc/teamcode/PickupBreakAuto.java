7bb124258ea188d6b3c594b1245666a9b5027503
Mohib Ibrahim <Ibrahimohib@users.noreply.github.com>
2024-12-23 06:16:59 -0800

    Update PickupBreakAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickupBreakAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickupBreakAuto.java
@@ -22,9 +22,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 public class PickupBreakAuto extends LinearOpMode {
-  
-  // MAKE SURE THAT THIS FLAG IS OFF FOR ROBOT TESTING.
-  private  final boolean USE_SIMULATOR = true;
+
+    // MAKE SURE THAT THIS FLAG IS OFF FOR ROBOT TESTING.
+    private  final boolean USE_SIMULATOR = true;
 
     /* Declare OpMode members. */
     private DcMotor         leftDrive   = null;
@@ -36,7 +36,7 @@ public class PickupBreakAuto extends LinearOpMode {
     private DcMotor m8;
     private DcMotor m5;
     private DcMotor m6releasesample;
- 
+
     private Servo m8Robot;
     private CRServo m6releasesampleRobot;
 
@@ -61,125 +61,177 @@ public class PickupBreakAuto extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-      
-      backRight = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? RIGHT_MOTOR_SIM : RIGHT_MOTOR_ROBOT);
-      backLeft = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? LEFT_MOTOR_SIM : LEFT_MOTOR_ROBOT);
-      m5 = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? ARM_MOTOR_SIM : ARM_MOTOR_ROBOT);
 
-      
-    if(USE_SIMULATOR){
-        m8 = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? WRIST_SIM : WRIST_ROBOT);
+        backRight = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? RIGHT_MOTOR_SIM : RIGHT_MOTOR_ROBOT);
+        backLeft = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? LEFT_MOTOR_SIM : LEFT_MOTOR_ROBOT);
         m5 = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? ARM_MOTOR_SIM : ARM_MOTOR_ROBOT);
-        m6releasesample = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? INTAKE_SIM : INTAKE_ROBOT);
-        leftDrive  = hardwareMap.get(DcMotor.class, "frontLeft");
-       rightDrive = hardwareMap.get(DcMotor.class, "frontRight");
-       leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);  
-       rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);  
-      }
 
-      else {
-        m8Robot = hardwareMap.get(Servo.class, USE_SIMULATOR ? WRIST_SIM : WRIST_ROBOT);
-       m6releasesampleRobot = hardwareMap.get(CRServo.class, USE_SIMULATOR ? INTAKE_SIM : INTAKE_ROBOT);
-      }
-      
-     
-        
-       backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-       backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-      
-      telemetry.addData("Status", "Initialized");
-      telemetry.update();
-      
-      waitForStart();
-      
-          //Go Straight
-    backLeft.setPower(1.0);
-    backRight.setPower(1.0);
-    sleep(100);  
-      
-    // Turn Right
-    backLeft.setPower(-1.0);
-    backRight.setPower(1.0);
-    sleep(1100);
 
-    // Stop - not sure if we need the stop in between each step - but added it
-    backLeft.setPower(0);
-    backRight.setPower(0);
-    sleep(100)  
-      
-    //Go Straight
-    backLeft.setPower(1.0);
-    backRight.setPower(1.0);
-    sleep(100);  
+        if(USE_SIMULATOR){
+            m8 = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? WRIST_SIM : WRIST_ROBOT);
+            m5 = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? ARM_MOTOR_SIM : ARM_MOTOR_ROBOT);
+            m6releasesample = hardwareMap.get(DcMotor.class, USE_SIMULATOR ? INTAKE_SIM : INTAKE_ROBOT);
+            leftDrive  = hardwareMap.get(DcMotor.class, "frontLeft");
+            rightDrive = hardwareMap.get(DcMotor.class, "frontRight");
+            leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+            rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
 
-    // Stop
-    backLeft.setPower(0);
-    backRight.setPower(0);
-    sleep(100)  
-      
-    // Turn Left
-    backLeft.setPower(1.0);
-    backRight.setPower(-1.0);
-    sleep(750);
+        else {
+            m8Robot = hardwareMap.get(Servo.class, USE_SIMULATOR ? WRIST_SIM : WRIST_ROBOT);
+            m6releasesampleRobot = hardwareMap.get(CRServo.class, USE_SIMULATOR ? INTAKE_SIM : INTAKE_ROBOT);
+        }
 
-     // Stop
-    backLeft.setPower(0);
-    backRight.setPower(0);
-    sleep(1);
- 
-    //Go Straight
-    backLeft.setPower(1.0);
-    backRight.setPower(1.0);
-    sleep(500);  
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
 
-    // Stop
-    backLeft.setPower(0);
-    backRight.setPower(0);
-    sleep(100)  
-     
-    // Turn Right
-    backLeft.setPower(-1.0);
-    backRight.setPower(1.0);
-    sleep(400);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-    // Stop
-    backLeft.setPower(0);
-    backRight.setPower(0);
-    sleep(100)  
-    
-    // Put the arm down  
-    m8.setTargetPosition(1));
-    m8.setPower(1));
-      
-    // Put the wrist into position  
-    m5.setTargetPosition(1);
-    m5.setPower(1);
-    sleep(6250);
-    m5.setPower(0);
-    m8.setPower(0);
-      
-    // Turn on motor to pick up sample
-    m6releasesample.setPower(1);
-      
-    //Go Straight to pick up sample
-    backLeft.setPower(1.0);
-    backRight.setPower(1.0);
-    sleep(300);  
-      
-    //Stop robot after sample picked up
-    backLeft.setPower(0);
-    backRight.setPower(00);
-    sleep(100);  
-      
-    m8.setPower(-1);
-    sleep(950);
-    m8.setPower(0);
+        waitForStart();
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Running");
+        //Go Straight
+        backLeft.setPower(1.0);
+        backRight.setPower(1.0);
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
+        // Turn Right
+        backLeft.setPower(-1.0);
+        backRight.setPower(1.0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        // Stop - not sure if we need the stop in between each step - but added it
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.1)) {
+            telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        //Go Straight
+        backLeft.setPower(1.0);
+        backRight.setPower(1.0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 4: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Stop
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 5: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Turn Left
+        backLeft.setPower(1.0);
+        backRight.setPower(-1.0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.1)) {
+            telemetry.addData("Path", "Leg 6: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Stop
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 7: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        //Go Straight
+        backLeft.setPower(1.0);
+        backRight.setPower(1.0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 8: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Stop
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 9: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Turn Right
+        backLeft.setPower(-1.0);
+        backRight.setPower(1.0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 10: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Stop
+        backLeft.setPower(0);
+        backRight.setPower(0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 11: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        // Put the arm down  
+        m8.setTargetPosition(1);
+        m8.setPower(1);
+
+        // Put the wrist into position  
+        m5.setTargetPosition(1);
+        m5.setPower(1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 7)) {
+            telemetry.addData("Path", "Leg 12: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        m5.setPower(0);
+        m8.setPower(0);
+
+        // Turn on motor to pick up sample
+        m6releasesample.setPower(1);
+
+        //Go Straight to pick up sample
+        backLeft.setPower(1.0);
+        backRight.setPower(1.0);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 13: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        //Stop robot after sample picked up
+        backLeft.setPower(0);
+        backRight.setPower(00);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            telemetry.addData("Path", "Leg 14: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+        m8.setPower(-1);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1)) {
+            telemetry.addData("Path", "Leg 15: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        m8.setPower(0);
+
+        // run until the end of the match (driver presses STOP)
+        telemetry.addData("Status", "Ending");
+        telemetry.update();
     }
-  
+
 }
