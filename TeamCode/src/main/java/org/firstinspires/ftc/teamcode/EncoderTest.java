5c13eea287aec6e61a9118276622fffaf4514fcb
Mohib Ibrahim <Ibrahimohib@users.noreply.github.com>
2024-12-25 12:57:51 -0800

    Update EncoderTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -1,21 +1,3 @@
-/*
-Copyright 2021 FIRST Tech Challenge Team 0000
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial
-portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -26,19 +8,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- * <p>
- * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all linear OpModes contain.
- * <p>
- * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
- * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
- */
 
 public class EncoderTest extends LinearOpMode {
     private final boolean USE_SIMULATOR = true;
@@ -59,17 +28,12 @@ public class EncoderTest extends LinearOpMode {
 
     private final ElapsedTime runtime = new ElapsedTime();
 
-
-    private final double FORWARD_SPEED_ROBOT = 0.4;
-    private final double TURN_SPEED_ROBOT = 0.5;
     private final String RIGHT_MOTOR_ROBOT = "right_front_drive";
     private final String LEFT_MOTOR_ROBOT = "left_front_drive";
     private final String ARM_MOTOR_ROBOT = "left_arm";
     private final String WRIST_ROBOT = "wrist";
     private final String INTAKE_ROBOT = "intake";
 
-    private final double FORWARD_SPEED_SIM = 1.0;
-    private final double TURN_SPEED_SIM = 1.0;
     private final String RIGHT_MOTOR_SIM = "backRight";
     private final String LEFT_MOTOR_SIM = "backLeft";
     private final String ARM_MOTOR_SIM = "m5";
@@ -88,11 +52,11 @@ public class EncoderTest extends LinearOpMode {
         setupEncoders();
         waitForStart();
 
-        goStraight(42, 1);
-        turnRight(40, 2);
-        moveArm(9, 3);
-        moveWrist(10, 4);
+        goStraight(50, 1);
+        turnRight(44, 2);
+        moveArmDown(10, 4);
         pickupSample();
+        goStraight(5, 1);
 
         telemetry.addData("Status", "Ended");
     }
@@ -110,8 +74,9 @@ public class EncoderTest extends LinearOpMode {
             rightDrive = hardwareMap.get(DcMotor.class, "frontRight");
             leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
             rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+            moveWrist(10,0);
         } else {
-            m8Robot = hardwareMap.get(Servo.class, USE_SIMULATOR ? WRIST_SIM : WRIST_ROBOT);
+//            m8Robot = hardwareMap.get(Servo.class, USE_SIMULATOR ? WRIST_SIM : WRIST_ROBOT);
             m6releasesampleRobot = hardwareMap.get(CRServo.class, USE_SIMULATOR ? INTAKE_SIM : INTAKE_ROBOT);
         }
 
@@ -190,7 +155,29 @@ public class EncoderTest extends LinearOpMode {
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    private void moveArm(int distance, int leg) {
+    private void moveArmDown(int distance, int leg) {
+        //For some reason the simulator does not accepted casted values.
+        if (!USE_SIMULATOR) {
+            target = (int) ((distance * 10 / circumference) * ticks);
+        } else {
+            //Comment this out if using robot.
+//            target = (distance * 100 / circumference) * (ticks) + m8.getCurrentPosition();
+        }
+        m8.setTargetPosition(target);
+        m8.setPower(1);
+        m8.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        while (runtime.seconds() < distance) {
+            telemetry.addData("Path", "Leg %s: Arm movement: Current position: %s Target Position:%s"
+                    , leg, runtime.seconds(), target);
+            telemetry.update();
+        }
+
+        // Reset encoders
+        m8.setPower(0);
+        m8.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    private void moveArmUp(int distance, int leg) {
         //For some reason the simulator does not accepted casted values.
         if (!USE_SIMULATOR) {
             target = (int) ((distance * 10 / circumference) * ticks);
@@ -218,13 +205,13 @@ public class EncoderTest extends LinearOpMode {
             target = (int) ((distance * 10 / circumference) * ticks);
         } else {
             //Comment this out if using robot.
-//            target = (distance * 100 / circumference) * (ticks) + m8.getCurrentPosition();
+//            target = (distance / circumference) * (ticks) + m8.getCurrentPosition();
         }
         m5.setTargetPosition(target);
         m5.setPower(1);
         m5.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         while (runtime.seconds() < distance) {
-            telemetry.addData("Path", "Leg 4: Wrist movement: Current position: %s Target Position:%s"
+            telemetry.addData("Path", "Leg %s: Wrist movement: Current position: %s Target Position:%s"
                     , leg, m5.getCurrentPosition(), m5.getTargetPosition());
             telemetry.update();
         }
@@ -236,6 +223,20 @@ public class EncoderTest extends LinearOpMode {
     }
 
     private void pickupSample() {
-        m6releasesample.setPower(1);
+        if(USE_SIMULATOR){
+            m6releasesample.setPower(1);
+        }
+        else{
+            m6releasesampleRobot.setPower(1);
+        }
+    }
+
+    private void dropSample() {
+        if(USE_SIMULATOR){
+            m6releasesample.setPower(-1);
+        }
+        else{
+            m6releasesampleRobot.setPower(-1);
+        }
     }
 }
