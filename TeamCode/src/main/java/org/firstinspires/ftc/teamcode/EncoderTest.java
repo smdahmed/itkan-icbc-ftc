c48c660bf0dc8221b117a256b4ea8832ba8909f1
Mohib Ibrahim <ibrahimohib@gmail.com>
2024-12-27 21:29:30 -0600

    Update EncoderTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Encoder Test", group = "Robot")
@@ -22,6 +23,14 @@ public class EncoderTest extends LinearOpMode {
     private final double ticks = 537.7;
     //Circumference is based on the wheel size.
     private final double circumference = 301.59;
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1/360.0; // we want ticks per degree, not per rotation
+
+    final double ARM_SCORE_SAMPLE_IN_HIGH  = 100 * ARM_TICKS_PER_DEGREE;
+
     private int target;
 
     @Override
@@ -30,7 +39,11 @@ public class EncoderTest extends LinearOpMode {
         setupEncoders();
         waitForStart();
 
-        goStraight(20);
+        goStraight(25);
+
+        moveArmUp(0);
+//        turnRight(50);
+
 
         //Commented out while we test.
 //        turnRight(600);
@@ -53,8 +66,8 @@ public class EncoderTest extends LinearOpMode {
         viperKit.setTargetPosition(0);
         viperKit.setPower(0.5);
 
-        backRight.setDirection(DcMotor.Direction.FORWARD);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARD);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
     }
@@ -70,10 +83,10 @@ public class EncoderTest extends LinearOpMode {
         target = (int) ((distance * 10 / circumference) * ticks);
         backRight.setTargetPosition(target);
         backLeft.setTargetPosition(target);
-        backRight.setPower(1);
-        backLeft.setPower(1);
         backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setPower(1);
+        backLeft.setPower(1);
         while (backRight.isBusy() && backLeft.isBusy()) {
             telemetry.addData("Path", "Going straight: Current position: %s Target Position:%s",
                     backRight.getCurrentPosition(), backRight.getTargetPosition());
@@ -82,6 +95,8 @@ public class EncoderTest extends LinearOpMode {
         // Reset encoders
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     private void turnRight(int distance) {
@@ -90,8 +105,8 @@ public class EncoderTest extends LinearOpMode {
 
         backRight.setTargetPosition(target);
         backLeft.setTargetPosition(target);
-        backRight.setPower(-1);
-        backLeft.setPower(1);
+        backRight.setPower(1);
+        backLeft.setPower(-1);
         backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         telemetry.update();
@@ -105,6 +120,8 @@ public class EncoderTest extends LinearOpMode {
         backLeft.setPower(0);
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     private void turnLeft(int distance) {
@@ -113,10 +130,10 @@ public class EncoderTest extends LinearOpMode {
 
         backRight.setTargetPosition(target);
         backLeft.setTargetPosition(target);
-        backRight.setPower(1);
-        backLeft.setPower(-1);
         backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setPower(1);
+        backLeft.setPower(-1);
         telemetry.update();
         while (isNotInPosition(backRight) && isNotInPosition(backLeft)) {
             telemetry.addData("Turning left", "Leg %s: Current position: %s Target Position:%s"
@@ -128,6 +145,8 @@ public class EncoderTest extends LinearOpMode {
         backLeft.setPower(0);
         backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     private void moveArmDown(int distance) {
@@ -152,7 +171,7 @@ public class EncoderTest extends LinearOpMode {
         //For some reason the simulator does not accepted casted values.
         target = (int) ((distance * 10 / circumference) * ticks);
 
-        arm.setTargetPosition(target);
+        arm.setTargetPosition((int) ARM_SCORE_SAMPLE_IN_HIGH);
         arm.setPower(0.5);
         arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         while (runtime.seconds() < distance) {
@@ -160,14 +179,12 @@ public class EncoderTest extends LinearOpMode {
                     , runtime.seconds(), target);
             telemetry.update();
         }
-
         // Reset encoders
         arm.setPower(0);
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     private void pickupSample() {
-
         robotSampleServo.setPower(1);
 
     }
