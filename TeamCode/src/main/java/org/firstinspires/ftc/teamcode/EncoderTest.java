4713c4b7ff14951dbd1d6280b2c1d822bd601c2c
Mohib Ibrahim <Ibrahimohib@users.noreply.github.com>
2024-12-31 22:12:26 -0600

    Update autonomous name.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "PickupSample", group = "Robot")
+@Autonomous(name = "HangSample", group = "Robot")
 public class EncoderTest extends LinearOpMode {
     public DcMotor backRight;
     public DcMotor backLeft;
